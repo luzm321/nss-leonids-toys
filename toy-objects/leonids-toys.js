035d6d058a517535329d6a8718a1cf17f9be7a72
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-05-27 15:12:18 -0500

    adding objects to existing toy array with push method and using for of loop exercise

--- toy-objects/leonids-toys.js
+++ toy-objects/leonids-toys.js
@@ -30,8 +30,38 @@ const toys = [
 
 // console.log(toys);
 
-// Displaying property of all toys:
+// Displaying the value of the inventory property of all toy objects in the toys array:
+
+// for (const toy of toys) {
+//     console.log(toy.inventory);
+// };
+
+//New Toys Exercise using the .push() method to add a new toy object in the toys array
+
+const barbie = {
+    id: 4,
+    name: "Barbie",
+    manufacturer: "Mattel, Inc.",
+    color: "pink",
+    inventory: 50,
+    price: 25,
+    isExpensive: true
+};
+
+const pogoStick = {
+    id: 5,
+    name: "Pogo Stick",
+    manufacturer: "Razor",
+    color: "Red",
+    inventory: 10,
+    price: 40,
+    isExpensive: true
+};
+
+toys.push(barbie);
+toys.push(pogoStick);
 
 for (const toy of toys) {
     console.log(toy.inventory);
 };
+
