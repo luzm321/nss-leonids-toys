669488a9b8af9cd1b27e815496d3c43fd2cb71bc
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-05-27 10:24:56 -0500

    my first version of leonids-toys that contains the toys array with three toy objects

---
+++ toy-objects/leonids-toys.js
@@ -0,0 +1,31 @@
+const toys = [
+    {
+        id: 1,
+        name: "Slinky",
+        manufacturer: "Alex Brands Inc.",
+        color: "green",
+        inventory: 21,
+        price: 2,
+        isExpensive: false
+    },
+    {
+        id: 2,
+        name: "Nerf Blaster",
+        manufacturer: "Hasbro",
+        color: "blue",
+        inventory: 30,
+        price: 30,
+        isExpensive: true
+    },
+    {
+        id: 3,
+        name: "Play-Doh",
+        manufacturer: "Hasbro",
+        color: "lilac",
+        inventory: 15,
+        price: 20,
+        isExpensive: false
+    }
+];
+
+console.log(toys);
